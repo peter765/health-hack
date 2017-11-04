1567a661252e55bb8e6898330ca9134a404da86f
Peter John <peterjohn512@outlook.com>
2017-11-04 02:53:11 -0400

    pointless fucking commit thanks heroku for not letting me restart a dyno from the dashboard

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 
-// Imports dependencies and set up http server
+// Imports dependencies and set up http server.
 const
   request = require('request'),
   express = require('express'),

0de0fc315e17c92e7fa271f36cd2b6a198dcb215
rkg2359 <rkg2359@gmail.com>
2017-11-04 11:30:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ var findProfile = function(db, callback) {
   db.collection('Patients',function (err,collection) {
     collection.find({"Name":"Peter","LastName":"John"},{"Name":1, "LastName":1,"DateOfBirth":1,"Ethnicity":1,"Address":1, "Allergies":1, "FamilyHistory":1, "PhoneNumber":1, "Height":1, "Weight":1}).toArray(function(err, results) {
       assert.equal(err, null);
-      ret = results[0].Name + " " + results[0].LastName + "\n" + results[0].DateOfBirth + "\n" + results[0].Ethnicity + "\n" + results[0].Address+ "\n" + results[0].Allergies + "\n" + results[0].FamilyHistory + "\n" + results[0].PhoneNumber + "\n" + results[0].Height + "\n" + results[0].Weight;  
+      let ret = results[0].Name + " " + results[0].LastName + "\n" + results[0].DateOfBirth + "\n" + results[0].Ethnicity + "\n" + results[0].Address+ "\n" + results[0].Allergies + "\n" + results[0].FamilyHistory + "\n" + results[0].PhoneNumber + "\n" + results[0].Height + "\n" + results[0].Weight;  
 
       console.log("Successful Profile");
       console.log(ret);

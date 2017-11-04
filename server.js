f602259253b2b8b983ac6b12a759175e06e015b1
Bharathi <bharathimurugan@gmail.com>
2017-11-04 14:21:43 -0400

    another damn comma

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ var findProfile = function(db, callback) {
             "template_type": "list",
             "top_element_style": "compact",
             "elements": [{
-              "title": results[0].LastName + ", " + results[0].Name,
+              "title": results[0].LastName + ", " + results[0].Name
               //"subtitle": "Date Of Birth: " + results[0].DateOfBirth + "\n" +"Ethnicity: " + results[0].Ethnicity + "\n" +"Address: " + results[0].Address + "\n" +"Phone Number: " + results[0].PhoneNumber + "\n" +"Height: " + results[0].Height + "\n" +"Weight:" + results[0].Weight
             },
             {

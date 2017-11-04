6ff5c5be991d8cf1492454416b9b7735e75372e7
bharathi108 <bharathimurugan@gmail.com>
2017-11-04 18:34:50 -0400

    today isnt fucking defined

--- server.js
+++ server.js
@@ -398,9 +398,9 @@ var addPresc = function(db, callback, firstName, lastName, date, value) {
 function handleMessage(sender_psid, received_message) {
   let response;
   var date = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
+  var dd = date.getDate();
+  var mm = date.getMonth()+1; //January is 0!
+  var yyyy = date.getFullYear();
 
   if(dd<10) {
     dd = '0'+dd

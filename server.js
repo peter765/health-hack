3cf1176be158347ae40c476fbb2837845a429b24
Bharathi <bharathimurugan@gmail.com>
2017-11-04 15:06:42 -0400

    submit

--- server.js
+++ server.js
@@ -216,7 +216,7 @@ function handleMessage(sender_psid, received_message) {
   // Checks if the message contains text
   if (received_message.text) {
 
-
+    //hello
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
       let text = JSON.stringify(received_message.nlp);

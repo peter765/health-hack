a6a0e66afe2d32ef0e90da2d9291d0768aafa325
Peter John <peterjohn512@outlook.com>
2017-11-04 19:15:43 -0400

    userapi

--- server.js
+++ server.js
@@ -626,9 +626,9 @@ function callSendAPI(sender_psid, response) {
 
 function callUserAPI(sender_psid) {
   //message body
-  console.log(sender_psid);
+  console.log("https://graph.facebook.com/v2.6/" + sender_psid);
   request({
-    "uri": "https://graph.facebook.com/v2.6/" + sender_psid,
+    "uri": "https://graph.facebook.com/v2.6/" + sender_psid + "?",
     "qs": {
       "fields": "first_name, last_name, profile_pic",
       "access_token": PAGE_ACCESS_TOKEN
